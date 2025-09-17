6a7367e6d0ad26ae29e20a1a711fce69755ae966
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-09-17 12:48:06 +0700

    update view file

---
+++ lap-trinh-web/src/main/java/com/example/Controller/User/Home/Home.java
@@ -0,0 +1,20 @@
+package com.example.Controller.User.Home;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        // forward sang file JSP
+        req.getRequestDispatcher("/user/index.jsp").forward(req, resp);
+    }
+}
